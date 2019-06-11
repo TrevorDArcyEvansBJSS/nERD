dcc32040bf1091fda0d8ad76fd947b28e0cb7c86
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-06-11 09:14:26 +0100

    - formatting
    - adjust case of some methods
    - code cleanup

--- src/DiagramEditor/ClassDiagram/Connections/Dependency.cs
+++ src/DiagramEditor/ClassDiagram/Connections/Dependency.cs
@@ -21,70 +21,70 @@ using NClass.DiagramEditor.ClassDiagram.Shapes;
 
 namespace NClass.DiagramEditor.ClassDiagram.Connections
 {
-	internal sealed class Dependency : Connection
-	{
-		static Pen linePen = new Pen(Color.Black);
+  internal sealed class Dependency : Connection
+  {
+    static Pen linePen = new Pen(Color.Black);
 
-		DependencyRelationship dependency;
+    DependencyRelationship dependency;
 
-		static Dependency()
-		{
-			linePen.MiterLimit = 2.0F;
-			linePen.LineJoin = LineJoin.MiterClipped;
-		}
+    static Dependency()
+    {
+      linePen.MiterLimit = 2.0F;
+      linePen.LineJoin = LineJoin.MiterClipped;
+    }
 
-		/// <exception cref="ArgumentNullException">
-		/// <paramref name="dependency"/> is null.-or-
-		/// <paramref name="startShape"/> is null.-or-
-		/// <paramref name="endShape"/> is null.
-		/// </exception>
-		public Dependency(DependencyRelationship dependency, Shape startShape, Shape endShape)
-			: base(dependency, startShape, endShape)
-		{
-			this.dependency = dependency;
-		}
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="dependency"/> is null.-or-
+    /// <paramref name="startShape"/> is null.-or-
+    /// <paramref name="endShape"/> is null.
+    /// </exception>
+    public Dependency(DependencyRelationship dependency, Shape startShape, Shape endShape)
+      : base(dependency, startShape, endShape)
+    {
+      this.dependency = dependency;
+    }
 
-		internal DependencyRelationship DependencyRelationship
-		{
-			get { return dependency; }
-		}
+    internal DependencyRelationship DependencyRelationship
+    {
+      get { return dependency; }
+    }
 
-		protected internal override Relationship Relationship
-		{
-			get { return dependency; }
-		}
+    protected internal override Relationship Relationship
+    {
+      get { return dependency; }
+    }
 
-		protected override bool IsDashed
-		{
-			get { return true; }
-		}
+    protected override bool IsDashed
+    {
+      get { return true; }
+    }
 
-		protected override Size EndCapSize
-		{
-			get { return Arrowhead.OpenArrowSize; }
-		}
+    protected override Size EndCapSize
+    {
+      get { return Arrowhead.OpenArrowSize; }
+    }
 
-		protected override void DrawEndCap(IGraphics g, bool onScreen, Style style)
-		{
-			linePen.Color = style.RelationshipColor;
-			linePen.Width = style.RelationshipWidth;
-			g.DrawLines(linePen, Arrowhead.OpenArrowPoints);
-		}
+    protected override void DrawEndCap(IGraphics g, bool onScreen, Style style)
+    {
+      linePen.Color = style.RelationshipColor;
+      linePen.Width = style.RelationshipWidth;
+      g.DrawLines(linePen, Arrowhead.OpenArrowPoints);
+    }
 
-		protected override bool CloneRelationship(Diagram diagram, Shape first, Shape second)
-		{
-			TypeBase firstType = first.Entity as TypeBase;
-			TypeBase secondType = second.Entity as TypeBase;
+    protected override bool CloneRelationship(Diagram diagram, Shape first, Shape second)
+    {
+      TypeBase firstType = first.Entity as TypeBase;
+      TypeBase secondType = second.Entity as TypeBase;
 
-			if (firstType != null && secondType != null)
-			{
-				DependencyRelationship clone = dependency.Clone(firstType, secondType);
-				return diagram.InsertDependency(clone);
-			}
-			else
-			{
-				return false;
-			}
-		}
-	}
+      if (firstType != null && secondType != null)
+      {
+        DependencyRelationship clone = dependency.Clone(firstType, secondType);
+        return diagram.InsertDependency(clone);
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
 }
--- src/DiagramEditor/ClassDiagram/Connections/Nesting.cs
+++ src/DiagramEditor/ClassDiagram/Connections/Nesting.cs
@@ -20,71 +20,71 @@ using NClass.DiagramEditor.ClassDiagram.Shapes;
 
 namespace NClass.DiagramEditor.ClassDiagram.Connections
 {
-	internal sealed class Nesting : Connection
-	{
-		const int Radius = 9;
-		const int Diameter = Radius * 2;
-		const int CrossSize = 8;
-		static Pen linePen = new Pen(Color.Black);
+  internal sealed class Nesting : Connection
+  {
+    const int Radius = 9;
+    const int Diameter = Radius * 2;
+    const int CrossSize = 8;
+    static Pen linePen = new Pen(Color.Black);
 
-		NestingRelationship nesting;
+    NestingRelationship nesting;
 
-		/// <exception cref="ArgumentNullException">
-		/// <paramref name="nesting"/> is null.-or-
-		/// <paramref name="startShape"/> is null.-or-
-		/// <paramref name="endShape"/> is null.
-		/// </exception>
-		public Nesting(NestingRelationship nesting, Shape startShape, Shape endShape)
-			: base(nesting, startShape, endShape)
-		{
-			this.nesting = nesting;
-		}
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="nesting"/> is null.-or-
+    /// <paramref name="startShape"/> is null.-or-
+    /// <paramref name="endShape"/> is null.
+    /// </exception>
+    public Nesting(NestingRelationship nesting, Shape startShape, Shape endShape)
+      : base(nesting, startShape, endShape)
+    {
+      this.nesting = nesting;
+    }
 
-		internal NestingRelationship NestingRelationship
-		{
-			get { return nesting; }
-		}
+    internal NestingRelationship NestingRelationship
+    {
+      get { return nesting; }
+    }
 
-		protected internal override Relationship Relationship
-		{
-			get { return nesting; }
-		}
+    protected internal override Relationship Relationship
+    {
+      get { return nesting; }
+    }
 
-		protected override Size StartCapSize
-		{
-			get { return new Size(Diameter, Diameter); }
-		}
+    protected override Size StartCapSize
+    {
+      get { return new Size(Diameter, Diameter); }
+    }
 
-		protected override int StartSelectionOffset
-		{
-			get { return Diameter; }
-		}
+    protected override int StartSelectionOffset
+    {
+      get { return Diameter; }
+    }
 
-		protected override void DrawStartCap(IGraphics g, bool onScreen, Style style)
-		{
-			linePen.Color = style.RelationshipColor;
-			linePen.Width = style.RelationshipWidth;
+    protected override void DrawStartCap(IGraphics g, bool onScreen, Style style)
+    {
+      linePen.Color = style.RelationshipColor;
+      linePen.Width = style.RelationshipWidth;
 
-			g.FillEllipse(Brushes.White, -Radius, 0, Diameter, Diameter);
-			g.DrawEllipse(linePen, -Radius, 0, Diameter, Diameter);
-			g.DrawLine(linePen, 0, Radius - CrossSize / 2, 0, Radius + CrossSize / 2);
-			g.DrawLine(linePen, -CrossSize / 2, Radius, CrossSize / 2, Radius);
-		}
+      g.FillEllipse(Brushes.White, -Radius, 0, Diameter, Diameter);
+      g.DrawEllipse(linePen, -Radius, 0, Diameter, Diameter);
+      g.DrawLine(linePen, 0, Radius - CrossSize / 2, 0, Radius + CrossSize / 2);
+      g.DrawLine(linePen, -CrossSize / 2, Radius, CrossSize / 2, Radius);
+    }
 
-		protected override bool CloneRelationship(Diagram diagram, Shape first, Shape second)
-		{
-			CompositeType firstType = first.Entity as CompositeType;
-			TypeBase secondType = second.Entity as TypeBase;
-			
-			if (firstType != null && secondType != null)
-			{
-				NestingRelationship clone = nesting.Clone(firstType, secondType);
-				return diagram.InsertNesting(clone);
-			}
-			else
-			{
-				return false;
-			}
-		}
-	}
+    protected override bool CloneRelationship(Diagram diagram, Shape first, Shape second)
+    {
+      CompositeType firstType = first.Entity as CompositeType;
+      TypeBase secondType = second.Entity as TypeBase;
+
+      if (firstType != null && secondType != null)
+      {
+        NestingRelationship clone = nesting.Clone(firstType, secondType);
+        return diagram.InsertNesting(clone);
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
 }
