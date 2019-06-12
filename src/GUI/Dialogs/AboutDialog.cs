fde04147f1be865e1e4b7219cc35d388cbd17571
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-06-12 16:26:05 +0100

    - added info for About box

--- src/GUI/Dialogs/AboutDialog.cs
+++ src/GUI/Dialogs/AboutDialog.cs
@@ -19,80 +19,74 @@ using NClass.Translations;
 
 namespace NClass.GUI.Dialogs
 {
-	public partial class AboutDialog : Form
-	{
-		public AboutDialog()
-		{
-			InitializeComponent();
-		}
+  public partial class AboutDialog : Form
+  {
+    public AboutDialog()
+    {
+      InitializeComponent();
+    }
 
-		private void UpdateTexts()
-		{
-			this.Text = Strings.AboutNClass;
-			lblTitle.Text = Program.GetVersionString();
-			lblCopyright.Text = "Copyright (C) 2006-2009 " + Strings.Author;
-			lblStatus.Text = string.Format(Strings.BetaVersion);
-			lnkEmail.Text = Strings.SendEmail;
-			lnkHomepage.Text = Strings.VisitHomepage;
-			btnClose.Text = Strings.ButtonClose;
+    private void UpdateTexts()
+    {
+      Text = Strings.AboutNClass;
+      lblTitle.Text = Program.GetVersionString();
+      lblCopyright.Text = "Copyright (C) 2006-2009 " + Strings.Author;
+      lblStatus.Text = string.Format(Strings.BetaVersion);
+      lnkEmail.Text = Strings.SendEmail;
+      lnkHomepage.Text = Strings.VisitHomepage;
+      btnClose.Text = Strings.ButtonClose;
 
-			lnkHomepage.Links.Clear();
-			lnkEmail.Links.Clear();
-			lnkHomepage.Links.Add(0, lnkHomepage.Text.Length, Properties.Resources.WebAddress);
-			lnkEmail.Links.Add(0, lnkEmail.Text.Length,
-				"mailto:" + Properties.Resources.MailAddress + "?subject=NClass");
-			lblTranslator.Text = Strings.Translator;
-		}
+      lnkHomepage.Links.Clear();
+      lnkEmail.Links.Clear();
+      lnkHomepage.Links.Add(0, lnkHomepage.Text.Length, Properties.Resources.WebAddress);
+      lnkEmail.Links.Add(0, lnkEmail.Text.Length, "mailto:" + Properties.Resources.MailAddress + "?subject=NClass");
+      lblTranslator.Text = Strings.Translator;
+    }
 
-		protected override void OnLoad(EventArgs e)
-		{
-			base.OnLoad(e);
-			UpdateTexts();
-		}
+    protected override void OnLoad(EventArgs e)
+    {
+      base.OnLoad(e);
+      UpdateTexts();
+    }
 
-		private void lnkEmail_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-		{
-			string target = e.Link.LinkData as string;
+    private void lnkEmail_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+      string target = e.Link.LinkData as string;
 
-			if (target != null)
-			{
-				try
-				{
-					System.Diagnostics.Process.Start(target);
-				}
-				catch (Exception ex)
-				{
-					MessageBox.Show(
-						string.Format("{0}\n{1}: {2}", Strings.CommandFailed,
-							Strings.ErrorsReason, ex.Message),
-						Strings.Error, MessageBoxButtons.OK, MessageBoxIcon.Error);
-				}
-			}
-		}
+      if (target != null)
+      {
+        try
+        {
+          System.Diagnostics.Process.Start(target);
+        }
+        catch (Exception ex)
+        {
+          MessageBox.Show(
+            string.Format("{0}\n{1}: {2}", Strings.CommandFailed,
+              Strings.ErrorsReason, ex.Message),
+            Strings.Error, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+      }
+    }
 
-		private void lnkHomepage_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-		{
-			string target = e.Link.LinkData as string;
+    private void lnkHomepage_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+      string target = e.Link.LinkData as string;
 
-			if (target != null)
-			{
-				try
-				{
-					System.Diagnostics.Process.Start(target);
-				}
-				catch (Exception ex)
-				{
-					MessageBox.Show(
-						string.Format("{0}\n{1}: {2}", Strings.CommandFailed,
-							Strings.ErrorsReason, ex.Message),
-						Strings.Error, MessageBoxButtons.OK, MessageBoxIcon.Error);
-				}
-			}
-		}
-
-		private void btnClose_Click(object sender, EventArgs e)
-		{
-			this.Close();
-		}
-	}
+      if (target != null)
+      {
+        try
+        {
+          System.Diagnostics.Process.Start(target);
+        }
+        catch (Exception ex)
+        {
+          MessageBox.Show(
+            string.Format("{0}\n{1}: {2}", Strings.CommandFailed,
+              Strings.ErrorsReason, ex.Message),
+            Strings.Error, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+      }
+    }
+  }
 }
--- src/GUI/Program.cs
+++ src/GUI/Program.cs
@@ -22,82 +22,68 @@ using NClass.Translations;
 
 namespace NClass.GUI
 {
-	internal static class Program
-	{
-		public static readonly Version CurrentVersion =
-			Assembly.GetExecutingAssembly().GetName().Version;
-		public static readonly string AppDataDirectory =
-			Path.Combine(Environment.GetFolderPath(
-			Environment.SpecialFolder.LocalApplicationData), "NClass");
+  internal static class Program
+  {
+    public static readonly Version CurrentVersion = Assembly.GetExecutingAssembly().GetName().Version;
+    public static readonly string AppDataDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "NClass");
 
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		private static void Main(string[] args)
-		{
-			CrashHandler.CreateGlobalErrorHandler();
-			UpdateSettings();
+    [STAThread]
+    private static void Main(string[] args)
+    {
+      CrashHandler.CreateGlobalErrorHandler();
+      UpdateSettings();
 
-			// Set the user interface language
-			UILanguage language = UILanguage.CreateUILanguage(Settings.Default.UILanguage);
-			if (language != null)
-				Strings.Culture = language.Culture;
+      // Set the user interface language
+      UILanguage language = UILanguage.CreateUILanguage(Settings.Default.UILanguage);
+      if (language != null)
+        Strings.Culture = language.Culture;
 
-			// Some GUI settings
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			ToolStripManager.VisualStylesEnabled = false;
+      // Some GUI settings
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      ToolStripManager.VisualStylesEnabled = false;
 
-			// Launch the application
-			LoadFiles(args);
-			Application.Run(new MainForm());
+      // Launch the application
+      LoadFiles(args);
+      Application.Run(new MainForm());
 
-			// Save application settings
-			DiagramEditor.Settings.Default.Save();
-			Settings.Default.Save();
-		}
+      // Save application settings
+      DiagramEditor.Settings.Default.Save();
+      Settings.Default.Save();
+    }
 
-		private static void UpdateSettings()
-		{
-			if (Settings.Default.CallUpgrade)
-			{
-				Settings.Default.Upgrade();
-				Settings.Default.CallUpgrade = false;
-			}
+    private static void UpdateSettings()
+    {
+      if (Settings.Default.CallUpgrade)
+      {
+        Settings.Default.Upgrade();
+        Settings.Default.CallUpgrade = false;
+      }
 
-			if (Settings.Default.OpenedProjects == null)
-				Settings.Default.OpenedProjects = new StringCollection();
-			if (Settings.Default.RecentFiles == null)
-				Settings.Default.RecentFiles = new StringCollection();
-		}
+      if (Settings.Default.OpenedProjects == null)
+        Settings.Default.OpenedProjects = new StringCollection();
+      if (Settings.Default.RecentFiles == null)
+        Settings.Default.RecentFiles = new StringCollection();
+    }
 
-		public static string GetVersionString()
-		{
-			if (CurrentVersion.Minor == 0)
-			{
-				return string.Format("NClass {0}.0", CurrentVersion.Major);
-			}
-			else
-			{
-				return string.Format("NClass {0}.{1:00}",
-					CurrentVersion.Major, CurrentVersion.Minor);
-			}
-		}
+    public static string GetVersionString()
+    {
+      return string.Format($"NClass {CurrentVersion.Major}.{CurrentVersion.Minor}.{CurrentVersion.Revision}");
+    }
 
-		private static void LoadFiles(string[] args)
-		{
-			if (args.Length >= 1)
-			{
-				foreach (string filePath in args)
-				{
-					Workspace.Default.OpenProject(filePath);
-				}
-			}
-			else if (Settings.Default.RememberOpenProjects)
-			{
-				Workspace.Default.Load();
-			}
-		}
-	}
+    private static void LoadFiles(string[] args)
+    {
+      if (args.Length >= 1)
+      {
+        foreach (string filePath in args)
+        {
+          Workspace.Default.OpenProject(filePath);
+        }
+      }
+      else if (Settings.Default.RememberOpenProjects)
+      {
+        Workspace.Default.Load();
+      }
+    }
+  }
 }
