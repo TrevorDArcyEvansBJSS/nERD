7a6dcaba873ff6198edbeff6d5d0115d26c02cc0
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-06-17 16:35:17 +0100

    - change exe name to nERD.exe

--- src/GUI/Properties/AssemblyInfo.cs
+++ src/GUI/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NClass")]
+[assembly: AssemblyTitle("nERD")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
