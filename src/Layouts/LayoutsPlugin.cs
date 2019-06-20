64f04782dd5bb8e71ef6f4d521a9ee212f87d2a7
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-06-20 10:24:58 +0100

    - skeleton layout plugin

---
+++ src/Layouts/LayoutsPlugin.cs
@@ -0,0 +1,45 @@
+﻿using Layouts.Lang;
+using Layouts.Properties;
+using NClass.GUI;
+using System;
+using System.Windows.Forms;
+
+namespace Layouts
+{
+  public sealed class LayoutsPlugin : Plugin
+  {
+    private readonly ToolStripMenuItem _menuItem;
+
+    public LayoutsPlugin(NClassEnvironment environment)
+      : base(environment)
+    {
+      _menuItem = new ToolStripMenuItem
+      {
+        Text = Strings.Menu_Title,
+        Image = Resources.Layouts_16,
+        ToolTipText = Strings.Menu_ToolTip
+      };
+      _menuItem.Click += MenuItem_Click;
+    }
+
+    public override bool IsAvailable
+    {
+      get { return DocumentManager.HasDocument; }
+    }
+
+    public override ToolStripItem MenuItem
+    {
+      get { return _menuItem; }
+    }
+
+    private void MenuItem_Click(object sender, EventArgs e)
+    {
+      if(!DocumentManager.HasDocument)
+      {
+        return;
+      }
+
+      // TODO   Layout
+    }
+  }
+}
