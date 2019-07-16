0613ccd8adc232e6e4b4b95bc8045bf774dabf3f
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-07-16 16:37:47 +0100

    - code cleanup

--- src/Core/Entities/State.cs
+++ src/Core/Entities/State.cs
@@ -13,7 +13,6 @@
 // this program; if not, write to the Free Software Foundation, Inc., 
 // 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-using NClass.Translations;
 using System.Xml;
 
 namespace NClass.Core
