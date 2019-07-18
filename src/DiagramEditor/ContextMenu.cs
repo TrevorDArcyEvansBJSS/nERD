7dc9fdb30b161518dc8540177de2968efd0694ad
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-07-18 14:29:30 +0100

    - refactored

--- src/DiagramEditor/ContextMenu.cs
+++ src/DiagramEditor/ContextMenu.cs
@@ -19,33 +19,28 @@ using System.Windows.Forms;
 
 namespace NClass.DiagramEditor
 {
-	public abstract class ContextMenu
-	{
-		internal static readonly ContextMenuStrip MenuStrip = new ContextMenuStrip();
+  public abstract class ContextMenu
+  {
+    internal static readonly ContextMenuStrip MenuStrip = new ContextMenuStrip();
 
-		List<ToolStripItem> menuItems = new List<ToolStripItem>();
+    public IEnumerable<ToolStripItem> GetMenuItems(IDocument document)
+    {
+      ValidateMenuItems(document);
+      return MenuList;
+    }
 
-		public IEnumerable<ToolStripItem> GetMenuItems(IDocument document)
-		{
-			ValidateMenuItems(document);
-			return menuItems;
-		}
+    protected abstract IDocument Document
+    {
+      get;
+    }
 
-		protected abstract IDocument Document
-		{
-			get;
-		}
+    internal IEnumerable<ToolStripItem> MenuItems
+    {
+      get { return MenuList; }
+    }
 
-		internal IEnumerable<ToolStripItem> MenuItems
-		{
-			get { return menuItems; }
-		}
+    protected List<ToolStripItem> MenuList { get; } = new List<ToolStripItem>();
 
-		protected List<ToolStripItem> MenuList
-		{
-			get { return menuItems; }
-		}
-
-		public abstract void ValidateMenuItems(IDocument document);
-	}
+    public abstract void ValidateMenuItems(IDocument document);
+  }
 }
