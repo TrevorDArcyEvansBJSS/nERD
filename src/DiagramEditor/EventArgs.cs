5d063a5d1bd66e38f04506e9c9e3d9f77dbd1b5a
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-07-22 15:31:37 +0100

    - refactored

--- src/DiagramEditor/EventArgs.cs
+++ src/DiagramEditor/EventArgs.cs
@@ -17,19 +17,13 @@ using System;
 
 namespace NClass.DiagramEditor
 {
-	public class EventArgs<T> : EventArgs
-	{
-		T value;
+  public sealed class EventArgs<T> : EventArgs
+  {
+    public T Value { get; set; }
 
-		public T Value
-		{
-			get { return this.value; }
-			set { this.value = value; }
-		}
-
-		public EventArgs(T value)
-		{
-			this.value = value;
-		}
-	}
+    public EventArgs(T value)
+    {
+      Value = value;
+    }
+  }
 }
