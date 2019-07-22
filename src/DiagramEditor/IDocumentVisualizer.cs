bd08c0e146c8e7772613572eb70c84f2ee2b8f5f
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-07-22 15:31:56 +0100

    - refactored

--- src/DiagramEditor/IDocumentVisualizer.cs
+++ src/DiagramEditor/IDocumentVisualizer.cs
@@ -15,37 +15,35 @@
 
 using System;
 using System.Drawing;
-using System.Windows.Forms;
 
 namespace NClass.DiagramEditor
 {
-	public interface IDocumentVisualizer
-	{
-		event EventHandler DocumentRedrawed;
-		event EventHandler VisibleAreaChanged;
+  public interface IDocumentVisualizer
+  {
+    event EventHandler DocumentRedrawed;
+    event EventHandler VisibleAreaChanged;
 
 
-		bool HasDocument { get; }
+    bool HasDocument { get; }
 
-		IDocument Document { get; }
+    IDocument Document { get; }
 
-		Point Offset { get; set; }
+    Point Offset { get; set; }
 
-		Size DocumentSize { get; }
+    Size DocumentSize { get; }
 
-		Rectangle VisibleArea { get; }
+    Rectangle VisibleArea { get; }
 
-		float Zoom { get; }
+    float Zoom { get; }
 
+    void ChangeZoom(bool enlarge);
 
-		void ChangeZoom(bool enlarge);
-		
-		void ChangeZoom(float zoom);
+    void ChangeZoom(float zoom);
 
-		void AutoZoom();
+    void AutoZoom();
 
-		void AutoZoom(bool selectedOnly);
+    void AutoZoom(bool selectedOnly);
 
-		void DrawDocument(Graphics g);
-	}
+    void DrawDocument(Graphics g);
+  }
 }
