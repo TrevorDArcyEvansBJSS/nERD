c50a67cd4de6d3d73b908242360f728a58a366ba
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-07-24 11:12:48 +0100

    - refactored

--- src/Core/Relationships/RealizationRelationship.cs
+++ src/Core/Relationships/RealizationRelationship.cs
@@ -64,8 +64,7 @@ namespace NClass.Core
 
     public override string ToString()
     {
-      return string.Format("{0}: {1} --> {2}",
-        Strings.Realization, First.Name, Second.Name);
+      return string.Format("{0}: {1} --> {2}", Strings.Realization, First.Name, Second.Name);
     }
   }
 }
