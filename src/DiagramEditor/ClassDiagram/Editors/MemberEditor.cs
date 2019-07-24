54509b36251b975961e695225e73809d4884bb1c
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-07-24 15:07:17 +0100

    - refactored

--- src/DiagramEditor/ClassDiagram/Editors/MemberEditor.cs
+++ src/DiagramEditor/ClassDiagram/Editors/MemberEditor.cs
@@ -13,17 +13,17 @@
 // this program; if not, write to the Free Software Foundation, Inc., 
 // 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-using System;
-using System.Drawing;
-using System.ComponentModel;
-using System.Windows.Forms;
 using NClass.Core;
 using NClass.DiagramEditor.ClassDiagram.Shapes;
 using NClass.Translations;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace NClass.DiagramEditor.ClassDiagram.Editors
 {
-  public partial class MemberEditor : FloatingEditor
+  public sealed partial class MemberEditor : FloatingEditor
   {
     public CompositeTypeShape Shape { get; set; } = null;
     public bool NeedValidation { get; private set; } = false;
