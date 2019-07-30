dfb65be4ff3376032589d943efa76ce7405c6a2d
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-07-30 09:01:29 +0100

    - refactored

--- src/CodeGenerator/CSharpSourceFileGenerator.cs
+++ src/CodeGenerator/CSharpSourceFileGenerator.cs
@@ -13,10 +13,9 @@
 // this program; if not, write to the Free Software Foundation, Inc., 
 // 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+using NClass.Core;
 using System;
 using System.Collections.Specialized;
-using NClass.Core;
-using NClass.CSharp;
 
 namespace NClass.CodeGenerator
 {
