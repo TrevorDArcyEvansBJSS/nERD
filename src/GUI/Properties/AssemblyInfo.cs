7fc9ba15d45371506a870c0e78e12b149265bbd7
Trevor D'Arcy-Evans <trevor.darcy-evans@bjss.com>
2019-08-08 10:54:06 +0100

    - update to v3.6.0

--- src/GUI/Properties/AssemblyInfo.cs
+++ src/GUI/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.5.0.2")]
+[assembly: AssemblyVersion("3.6.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
